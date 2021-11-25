ba87d99cdc7234263c4a854f93df5870d4cd4c78
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-11-25 16:33:09 -0500

    Challenge 2nd Completed: set state for both numbers in the function so can be change at the same time.

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class _DicePageState extends State<DicePage> {
               onPressed: () {
                 setState(() {
                   leftDiceNumber = Random().nextInt(6)+1;
+                  rightDiceNumber = Random().nextInt(6)+1;
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
@@ -56,6 +57,7 @@ class _DicePageState extends State<DicePage> {
             child: TextButton(
               onPressed: () {
                 setState(() {
+                  leftDiceNumber = Random().nextInt(6)+1;
                   rightDiceNumber = Random().nextInt(6)+1;
                 });
               },
