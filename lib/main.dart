79439c8ff28d0e4634c4401f4a3e6c98997a13f3
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-11-25 16:27:02 -0500

    Challenge completed: set state for right dice.

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -54,9 +55,11 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right button has been pressed.');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6)+1;
+                });
               },
-              child: Image.asset('images/dice2.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
